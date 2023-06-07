b40e3e5e43403aa115687cde5647de32ee6857e3
DoAnAileronRoll <60230545+DoAnAileronRoll@users.noreply.github.com>
2023-06-06 23:02:22 -0400

    Update box.js

--- box.js
+++ box.js
@@ -353,7 +353,8 @@ body {
 
             //this.addNewSection("NAVIGATION", "GRAHHHH")
             //document.getElementsByTagName("com-cbeyondata-sidepanel")[0].shadowRoot.getElementById("tableNavigation") is parent name if it exists
-            document.getElementsByTagName("com-cbeyondata-sidepanel")[0].shadowRoot.children.forEach(shadowNode =>{
+            for(var num = 0; num < document.getElementsByTagName("com-cbeyondata-sidepanel")[0].shadowRoot.children.length; num++){
+                shadowNode = document.getElementsByTagName("com-cbeyondata-sidepanel")[0].shadowRoot.children[num]
                 if(shadowNode.tagName != null && shadowNode.tagName == "TABLE"){
                     this.addNewSection(shadowNode.id,"this is a section")
                     var table = document.getElementsByTagName("com-cbeyondata-sidepanel")[0].shadowRoot.getElementById("tableNavigation")
@@ -386,7 +387,7 @@ body {
                         console.log(table.rows[i].cells[0].id)
                     }
                 }
-            })
+            }
         }
 		othersPrep(){
             
