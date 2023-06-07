01a588db249747684c990be0b34d5c30ec64c8b1
DoAnAileronRoll <60230545+DoAnAileronRoll@users.noreply.github.com>
2023-06-06 22:05:08 -0400

    Update box.js

--- box.js
+++ box.js
@@ -364,7 +364,7 @@ body {
                     if(table.rows[i+1].getAttribute("parentname") != null){
                         var parentIndex = i
                         this.addNewHier(table.rows[i].cells[0].id, "I am crying")
-                        while(table.rows[i+1].getAttribute("parentname") != null){
+                        while(i<table.rows.length-1 && table.rows[i+1].getAttribute("parentname") != null){
                             this.addToHier(table.rows[parentIndex].cells[0].id, this.createNewChild(table.rows[i+1].cells[0].id,"A test","pray for me"))
                             i++
                         }
