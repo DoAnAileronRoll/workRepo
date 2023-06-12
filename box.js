b29b540524df48364fb25dbac7e769e93efdfc99
DoAnAileronRoll <60230545+DoAnAileronRoll@users.noreply.github.com>
2023-06-12 11:28:27 -0400

    Update box.js

--- box.js
+++ box.js
@@ -366,7 +366,7 @@ body {
 	class Box extends HTMLElement {
         getFullEditSideBar(){
             const returnList = [] 
-            const curList = []
+            var curList
             var mainSideBar = this.shadowRoot.getElementById("mainSideBar").children
             for(var i=0;i<mainSideBar.length;i++){
                 if(mainSideBar[i].classList.contains("hierarchy")){
