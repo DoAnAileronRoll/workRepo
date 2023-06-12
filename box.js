99b21c1815ab356604c892c296cdb4dd1c22fac0
DoAnAileronRoll <60230545+DoAnAileronRoll@users.noreply.github.com>
2023-06-12 11:04:22 -0400

    Update box.js

--- box.js
+++ box.js
@@ -367,7 +367,6 @@ body {
         getFullEditSideBar(){
             const returnList = [] 
             const curList = []
-            var track = 0
             var mainSideBar = this.shadowRoot.getElementById("mainSideBar").children
             for(var i=0;i<mainSideBar.length;i++){
                 if(mainSideBar[i].classList.contains("hierarchy")){
@@ -376,15 +375,15 @@ body {
                     curList[0]=mainSideBar[i].id
                     curList[1]=mainSideBar[i].getAttribute("description")
                     curList[2]=mainSideBar[i].getAttribute("link")
-                    returnList[track]=curList
-                    track++
+                    returnList.push("curList")
+                    
                     for(var j=1;j<mainSideBar[i].children[1].children.length;j++){
                         //ADD CHECK FOR EMPTY LIST
                         curList[0] = mainSideBar[i].children[1].children[j].id
                         curList[1] = mainSideBar[i].children[1].children[j].getAttribute("description")
                         curList[2] = mainSideBar[i].children[1].children[j].getAttribute("link")
-                        returnList[track]=curList
-                        track++
+                        returnList.push("curList")
+                       
                         //returnList.append("child "+this.shadowRoot.getElementById("mainSideBar").children[2].children[1].children[j].id)
                         //console.log("child "+this.shadowRoot.getElementById("mainSideBar").children[2].children[1].children[j].id)
                     }
@@ -395,8 +394,8 @@ body {
                     curList[0]=mainSideBar[i].id
                     curList[1]=mainSideBar[i].getAttribute("description")
                     curList[2]=mainSideBar[i].getAttribute("link")
-                    returnList[track]=curList
-                    track++
+                    returnList.push("curList")
+           
                 }
                 else{
                     //returnList.append("standalone "+mainSideBar[i].id)
@@ -404,8 +403,8 @@ body {
                     curList[0]=mainSideBar[i].id
                     curList[1]=mainSideBar[i].getAttribute("description")
                     curList[2]=mainSideBar[i].getAttribute("link")
-                    returnList[track]=curList
-                    track++
+                    returnList.push("curList")
+    
                 }
             }
             console.log("GRAHHHHHHHHHHHH"+ returnList)
